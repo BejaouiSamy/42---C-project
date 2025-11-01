a9882ccb456a5272c2ca45f9606272edff638bd2
Samy <161335330+BejaouiSamy@users.noreply.github.com>
2025-11-01 15:39:54 +0100

    Delete cpp_module_01/cpp_module_01 directory

--- cpp_module_01/cpp_module_01/ex00/src/newZombie.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/Zombie.hpp"
-
-Zombie *heap_zombie(std::string name)
-{
-    return new Zombie(name);
-}
--- cpp_module_01/cpp_module_01/ex00/src/randomChump.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/Zombie.hpp"
-
-Zombie stack_zombie(std::string name)
-{
-    return name;
-}
