dcd505764f0959011dfe7bfd6ff350885e6661ff
Samy <161335330+BejaouiSamy@users.noreply.github.com>
2025-11-01 15:35:34 +0100

    Add files via upload

---
+++ cpp_module_01/ex05/main.cpp
@@ -0,0 +1,14 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERRORS");
+    harl.complain("RANDOM"); // test
+
+    return (0);
+}
